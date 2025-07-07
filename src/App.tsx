249964067a942c04a18b2751c41815175735c381
Zahid Díaz <152237386+LockHurb@users.noreply.github.com>
2025-07-07 18:57:30 +0000

    Configuracion de useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
 
 function App() {
    return (
       <Grid container spacing={5} justifyContent="center" alignItems="center">
 
          {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado
+            <HeaderUI />
+         </Grid>
 
          {/* Alertas */}
-         <Grid>Elemento: Alertas</Grid>
+         <Grid container justifyContent="right" alignItems="center">Elemento: Alertas
+            <AlertUI description="No se preveen lluvias" />
+         </Grid>
 
          {/* Selector */}
-         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector
+            <SelectorUI />
+         </Grid>
 
          {/* Indicadores */}
          <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
